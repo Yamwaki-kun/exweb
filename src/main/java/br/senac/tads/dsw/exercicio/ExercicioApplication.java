16c945c2e91cbfc51d1d689e3c93b9aed46beb60
Arnaldo <arnaldoalves34@gmail.com>
2021-12-02 21:09:25 -0300

    sprint 6

--- src/main/java/br/senac/tads/dsw/exercicio/ExercicioApplication.java
+++ src/main/java/br/senac/tads/dsw/exercicio/ExercicioApplication.java
@@ -12,44 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@RestController
-@ComponentScan(basePackages = {"com.*"})
+@ComponentScan(basePackages="br.senac.tads.dsw.resources")
 public class ExercicioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExercicioApplication.class, args);
     }
 
-    @Autowired
-    private PessoasService pessoae;
-
-    @RequestMapping("/pessoas")
-    public ModelAndView mostrar() {
-        ModelAndView mv = new ModelAndView("lpessoas");
-        return mv;
-    }
-
-    @RequestMapping("/pessoas/add-new")
-    public ModelAndView formulario() {
-        ModelAndView mv = new ModelAndView("formulario");
-        return mv;
-    }
-
-    @RequestMapping("/salvar")
-    public ModelAndView salvarDados(@RequestParam("nome") String nome, @RequestParam("email") String email, @RequestParam("datanasc") String datanasc) {
-        System.out.println(nome);
-        System.out.println(email);
-        System.out.println(datanasc);
-        Pessoas p = new Pessoas();
-        p.setNome(nome);
-        p.setEmail(email);
-        p.setDatanasc(LocalDate.parse(datanasc));
-        pessoae.insertCliente(p);
-        ModelAndView mv = new ModelAndView("lpessoas");
-        return mv;
-    }
+    
 
 }
--- src/main/java/br/senac/tads/dsw/resources/PessoasController.java
+++ src/main/java/br/senac/tads/dsw/resources/PessoasController.java
@@ -7,14 +7,17 @@ package br.senac.tads.dsw.resources;
 
 import br.senac.tads.dsw.entities.Pessoas;
 import br.senac.tads.dsw.services.PessoasService;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  *
@@ -22,27 +25,34 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @RestController
 @RequestMapping(value = "/pessoas")
-public class PessoasResource {
+@ComponentScan
+public class PessoasController {
     @Autowired
-    private PessoasService service;
-            
-    @GetMapping
-    public ModelAndView cadastrar() {
-        System.out.println("Minha rola");
+    private PessoasService pessoae;
+
+    @RequestMapping()
+    public ModelAndView mostrar() {
         ModelAndView mv = new ModelAndView("lpessoas");
         return mv;
-        
-
     }
 
-    @PostMapping(value = "/cadastrar")
-    public ModelAndView salvardados(){
+    @RequestMapping("/add-new")
+    public ModelAndView formulario() {
         ModelAndView mv = new ModelAndView("formulario");
         return mv;
     }
-    @GetMapping(value ="/add-new")
-    public ModelAndView verForm(){
-        ModelAndView mv = new ModelAndView("formulario");
+
+    @RequestMapping("/salvar")
+    public ModelAndView salvarDados(@RequestParam("nome") String nome, @RequestParam("email") String email, @RequestParam("datanasc") String datanasc) {
+        System.out.println(nome);
+        System.out.println(email);
+        System.out.println(datanasc);
+        Pessoas p = new Pessoas();
+        p.setNome(nome);
+        p.setEmail(email);
+        p.setDatanasc(LocalDate.parse(datanasc));
+        pessoae.insertCliente(p);
+        ModelAndView mv = new ModelAndView("lpessoas");
         return mv;
     }
 }
